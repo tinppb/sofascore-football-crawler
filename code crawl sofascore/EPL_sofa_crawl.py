cab60ca6159746f5a8e705509c22f5793c625d2a
Bao Tin <baotinphamphuoc@gmail.com>
2025-04-04 11:41:21 +0700

    tinppb: Remove unused dependency

--- code crawl sofascore/EPL_sofa_crawl.py
+++ code crawl sofascore/EPL_sofa_crawl.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
 
 # 🟢 **Khởi tạo trình duyệt Selenium**
